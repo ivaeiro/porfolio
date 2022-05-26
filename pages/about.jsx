2d6d6f86136b4c0c63bbbdeca6708c66edf63ce7
ivaeiro <palaciopeiroivan@gmail.com>
2022-05-27 00:22:58 +0200

    successfully builded

--- pages/about.jsx
+++ pages/about.jsx
@@ -19,16 +19,16 @@ function About() {
           querido ser programador.
         </p>
         <p>
-          Más tarde decidí decidí hacer programación y estudie el Ciclo
+          Más tarde decidí hacer programación y estudie el Ciclo
           Formativo de Grado Medio de SMR (Sistemas Microínformaticos y Redes),
-          ahora mismo, soy Tecnico en Sistemas Microínformaticos y Redes para
-          después hacer Ciclo Formativo de Grado Superior DAM(Desarrollo de
+          ahora mismo, soy Técnico en Sistemas Microínformaticos y Redes para
+          después hacer Ciclo Formativo de Grado Superior DAM (Desarrollo de
           Aplicaciones Multiplataforma).
         </p>
         <p>
           Si necesitas cualquier tipo de reparaciones, formateo e instalación de
-          distintos tipos de sistema operativos no dudes en contactar conmigo
-          estare encantado de ayudarte palaciopeiroivan@gmail.com.
+          distintos tipos de sistema operativos no dudes en contactar conmigo,
+          estare encantado de ayudarte, este es mi correo palaciopeiroivan@gmail.com.
         </p>
       </ContainerMain>
       <Footer />
