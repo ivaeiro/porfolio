baf2bd424c51563dd7eede45886f4ccbd15a39ee
ivaeiro <palaciopeiroivan@gmail.com>
2022-05-27 00:37:15 +0200

    successfully builded

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -113,33 +113,35 @@ function Navbar() {
             {(ref) => (
               <div className="md:hidden" id="mobile-menu">
                 <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  <Link href="../pages/inicio.jsx">
                   <a
-                    href="../pages/inicio.jsx"
                     className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     Inicio
                   </a>
-  
+                  </Link>
+                  <Link href="../pages/linux.jsx" passHref>
                   <a
                     href="../pages/linux.jsx"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     Linux
                   </a>
-  
+                  </Link>
+                  <Link href="../pages/windows.jsx" passHref>
                   <a
-                    href="../pages/windows.jsx"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     Windows
                   </a>
-  
+                  </Link>
+                  <Link href="../pages/about.jsx" passHref>
                   <a
-                    href="../pages/about.jsx"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
                     Sobre mí
                   </a>
+                  </Link>
                 </div>
               </div>
             )}
