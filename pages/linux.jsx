ae41377c1c6fdf7f837b7d2161616c8e9cd32a25
ivaeiro <palaciopeiroivan@gmail.com>
2022-05-27 00:10:08 +0200

    successfully builded

--- pages/linux.jsx
+++ pages/linux.jsx
@@ -14,7 +14,7 @@ function linux() {
       <div className="grid grid-cols-2 justify-center items-center">
           <div className="flex gap-2 justify-center items-center flex-col">
             <h1 className="text-2xl font-bold">Monitorizar clientes con Graylog para Ubuntu 20.04</h1>
-            <Image className="w-1/3" src="https://imgur.com/kgua13L.jpg" alt="Graylog" width={250} height={250}>
+            <Image className="w-1/3" src="https://imgur.com/kgua13L.jpg" alt="Graylog" width={300} height={200}>
             </Image>
             <Link href="/graylog" passHref>
             <button className="p-4 border-2 border-black rounded-full">Ir a Graylog</button>
@@ -25,7 +25,7 @@ function linux() {
       <div className="grid grid-cols-2 justify-center items-center">
           <div className="flex gap-2 justify-center items-center flex-col">
             <h1 className="text-2xl font-bold">Conexion remota Windows Ubuntu</h1>
-            <Image className="w-1/3" src="https://imgur.com/kgua13L.jpg" alt="Conexion remota Windows - Linux" width={250} height={250}>
+            <Image className="w-1/3" src="https://imgur.com/Um1Tnce.jpg" alt="Conexion remota Windows - Linux" width={200} height={200}>
             </Image>
             <Link href="/conexionRemotaWL" passHref>
             <button className="p-4 border-2 border-black rounded-full">Ir a conexion remota Windows - Linux</button>
--- pages/windows.jsx
+++ pages/windows.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import {Navbar, Footer, ContainerMain, Code} from '../components'
 import Link from 'next/link'
+import Image from 'next/image'
 
 function windows() {
   return (
@@ -12,8 +13,8 @@ function windows() {
     </Head>
     <div className="grid grid-cols-2 justify-center items-center">
           <div className="flex gap-2 justify-center items-center flex-col">
-            <img className="w-1/3" src="https://imgur.com/FperofC.jpg" alt="PowerShell">
-            </img>
+            <Image className="w-1/3" src="https://imgur.com/FperofC.jpg" alt="PowerShell" width={200} height={150}>
+            </Image>
             <h1 className="text-2xl font-bold">PowerShell</h1>
             <Link href="/PS" passHref>
             <button className="p-4 border-2 border-black rounded-full">Ir a PowerShell</button>
@@ -23,8 +24,8 @@ function windows() {
       </div>
       <div className="grid grid-cols-2 justify-center items-center">
           <div className="flex gap-2 justify-center items-center flex-col">
-            <img className="w-1/3" src="https://imgur.com/MlKdGZH.png" alt="CMD">
-            </img>
+            <Image className="w-1/3" src="https://imgur.com/MlKdGZH.png" alt="CMD" width={200} height={200}>
+            </Image>
             <h1 className="text-2xl font-bold">CMD</h1>
             <Link href="/cmd" passHref>
             <button className="p-4 border-2 border-black rounded-full">Ir a CMD</button>
@@ -33,8 +34,8 @@ function windows() {
       </div>
       <div className="grid grid-cols-2 justify-center items-center">
           <div className="flex gap-2 justify-center items-center flex-col">
-            <img className="w-1/3" src="https://imgur.com/d9lwhl3.png" alt="Git">
-            </img>
+            <Image className="w-1/3" src="https://imgur.com/d9lwhl3.png" alt="Git" width={250} height={250}>
+            </Image>
             <h1 className="text-2xl font-bold">Git</h1>
             <Link href="/git" passHref>
             <button className="p-4 border-2 border-black rounded-full">Ir a Git</button>
@@ -43,8 +44,8 @@ function windows() {
       </div>
       <div className="grid grid-cols-2 justify-center items-center">
           <div className="flex gap-2 justify-center items-center flex-col">
-            <img className="w-1/3" src="https://imgur.com/D1S4Dtc.jpg" alt="Quitar contraseña de windows">
-            </img>
+            <Image className="w-1/3" src="https://imgur.com/D1S4Dtc.jpg" alt="Quitar contraseña de windows" width={200} height={200}>
+            </Image>
             <h1 className="text-2xl font-bold">Quitar contraseña de windows</h1>
             <Link href="/quitarPassword" passHref>
             <button className="p-4 border-2 border-black rounded-full">Ir a quitar contraseña de windows</button>
