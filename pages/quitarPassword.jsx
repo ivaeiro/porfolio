96de4f8830c51346ec83172f17d97a3358a44fc3
ivaeiro <palaciopeiroivan@gmail.com>
2022-05-27 20:24:35 +0200

    successfully builded

--- pages/quitarPassword.jsx
+++ pages/quitarPassword.jsx
@@ -15,7 +15,11 @@ function quitarPassword() {
           <h1 className="text-3xl text-center font-bold p-2">
             ¿Como quitar la contraseña del windows?
           </h1>
-          <p>Para descargar la ISO de Hirens Boot, la descargaremos desde la pagina oficial que es <a href="https://www.hirensbootcd.org/">hirens Boot</a></p>
+          <p>
+            Pasos a seguir para restablecer la contraseña de un usuario usando
+            una ISO de Hiren's Boot.
+          </p>
+          <p>Para descargar la ISO de Hiren's Boot, la descargaremos desde la página oficial que es, <a href="https://www.hirensbootcd.org/">hiren's Boot</a></p>
         </ContainerMain>
       </main>
       <Footer />
