3cd548498845035fd4b50863fded637f1a3ceb40
ivaeiro <palaciopeiroivan@gmail.com>
2022-05-28 23:05:11 +0200

    successfully builded

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
-import {Navbar, Footer} from '../components'
+import {Navbar, Footer, FlipCard} from '../components'
 
 
 export default function Home() {
